ace8e5de5778868bf3ee52ff824b86ebf52923f1
Michał Gorzałczany <michal@gorzalczany.pl>
2023-11-02 09:57:01 +0100

    remove redundant config fields

--- src/configuration.ts
+++ src/configuration.ts
@@ -2,8 +2,6 @@ import { Service } from 'typedi';
 
 @Service()
 class Configuration {
-  public readonly DISCORD_CHANNEL_ID: string | null;
-  public readonly DISCORD_CLIENT_ID: string | null;
   public readonly DISCORD_GUILD_ID: string | null;
   public readonly DISCORD_TOKEN: string | null;
   public readonly UNAUTHORIZED_SOCKET: string | null;
@@ -11,16 +9,12 @@ class Configuration {
   public readonly PORT: string;
 
   private constructor(
-    DISCORD_CHANNEL_ID: string | null,
-    DISCORD_CLIENT_ID: string | null,
     DISCORD_GUILD_ID: string | null,
     DISCORD_TOKEN: string | null,
     UNAUTHORIZED_SOCKET: string | null,
     LOCALE: string,
     PORT: string,
   ) {
-    this.DISCORD_CHANNEL_ID = DISCORD_CHANNEL_ID;
-    this.DISCORD_CLIENT_ID = DISCORD_CLIENT_ID;
     this.DISCORD_GUILD_ID = DISCORD_GUILD_ID;
     this.DISCORD_TOKEN = DISCORD_TOKEN;
     this.UNAUTHORIZED_SOCKET = UNAUTHORIZED_SOCKET;
@@ -30,8 +24,6 @@ class Configuration {
 
   public static readFromEnv(): Configuration {
     return new Configuration(
-      process.env.DISCORD_CHANNEL_ID || null,
-      process.env.DISCORD_CLIENT_ID || null,
       process.env.DISCORD_GUILD_ID || null,
       process.env.DISCORD_TOKEN || null,
       process.env.UNAUTHORIZED_SOCKET || null,
